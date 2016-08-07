89d2df89dde840ff6c8242066842ff86af7cc5a2
Richard Chien <richardchienthebest@gmail.com>
2016-08-07 20:05:41 +0800

    Fix support_jsonp decorator

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def support_jsonp(f):
     def decorated_function(*args, **kwargs):
         callback = request.args.get('callback', False)
         if callback:
-            content = str(callback) + '(' + str(f(*args, **kwargs).data)[1:] + ')'
+            content = str(callback) + '(' + f(*args, **kwargs).data.decode('utf-8') + ')'
             return current_app.response_class(content, mimetype='application/json')
         else:
             return f(*args, **kwargs)
